ec2702fcb7e6f6e82104655d296103c6fdb947ab
Ned Redmond <nedredmond@pm.me>
2023-10-24 22:18:24 -0400

    adjust tests

--- src/utils/loop-index.test.ts
+++ src/utils/loop-index.test.ts
@@ -3,31 +3,19 @@ import loopIndex from "./loop-index";
 
 describe("loopIndex", () => {
   it("should return 0 when given an empty array", () => {
-    expect(loopIndex([], 0)).toBe({
-      index: 0,
-      dir: null,
-    });
+    expect(loopIndex([], 0)).toBe(0);
   });
 
   it("should return 0 when given an index of 0", () => {
-    expect(loopIndex(["a", "b", "c"], 0)).toBe({
-      index: 0,
-      dir: null,
-    });
+    expect(loopIndex(["a", "b", "c"], 0)).toBe(0);
   });
 
   it("should return 1 when given an index of 3 and an array of length 2", () => {
-    expect(loopIndex(["a", "b"], 3)).toBe({
-      index: 1,
-      dir: "right",
-    });
+    expect(loopIndex(["a", "b"], 3)).toBe(1);
   });
 
   it("should return 2 when given an index of -1 and an array of length 3", () => {
-    expect(loopIndex(["a", "b", "c"], -1)).toBe({
-      index: 2,
-      dir: "left",
-    });
+    expect(loopIndex(["a", "b", "c"], -1)).toBe(2);
   });
 
   it("should return 2 when given an index of -4 and an array of length 3", () => {
